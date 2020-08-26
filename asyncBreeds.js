6e41a7eb16baf420841643332b69afe61d6c55e9
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-08-26 19:57:21 +0000

    Asynchronous function return calls

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+// we try to get the return value
+const callback = function (data) {
+  console.log('Return Value: ', data);
+}
+
+breedDetailsFromFile('Bombay', callback);
